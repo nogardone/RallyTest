e5a65d27df1e45d7f554ca08f0a2713f81fd520c
OpenShift guest <amarinov2000@yahoo.com>
2014-06-06 05:12:07 -0400

    the big bang commit

---
+++ App.js
@@ -0,0 +1,8 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items:{ html:'Rallyto e gouema rabota'},
+    launch: function() {
+        console.log('Hello rally world MF 2');
+    }
+});
